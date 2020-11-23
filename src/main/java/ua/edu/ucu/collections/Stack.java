1a98c65d3c4ee2fedeec10f527bd9cef9259ba71
Danil1702 <60688896+Danil1702@users.noreply.github.com>
2020-11-23 23:15:50 +0200

    Update Stack.java

--- src/main/java/ua/edu/ucu/collections/Stack.java
+++ src/main/java/ua/edu/ucu/collections/Stack.java
@@ -5,25 +5,25 @@ import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
 public class Stack {
     private ImmutableLinkedList stack;
 
-    public Stack(){
+    public Stack() {
         this.stack = new ImmutableLinkedList();
     }
 
-    public Object peek(){
+    public Object peek() {
         return stack.getLast();
     }
 
-    public Object pop(){
+    public Object pop() {
         Object obj = peek();
         stack = stack.removeLast();
         return obj;
     }
 
-    public void push(Object e){
+    public void push(Object e) {
         stack = stack.addLast(e);
     }
 
-    public String toString(){
+    public String toString() {
         return stack.toString();
     }
 }
